b4063b0fab01f5a6528622470bf38e1e89bea26b
Zachary Friedrich <Zachf510@gmail.com>
2018-01-13 14:04:08 -0800

    Started New FPS Game
    also removed useless folder in section 3 to make gitignore cleaner and conent easier to access

---
+++ StealthGame/Source/FPSGame/Private/BlackHole.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BlackHole.h"
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+ABlackHole::ABlackHole()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABlackHole::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABlackHole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABlackHole::NotifyActorBeginOverlap(AActor * OtherActor)
+{
+
+
+}
+
